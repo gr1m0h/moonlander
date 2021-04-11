214e413d30a595e76046d8b6c86665722d7ac888
grimoh <dev@grimoh.net>
2021-04-11 15:48:22 +0900

    Update

--- moonlander_gr1m0h-s-layout_mzdoX/moonlander_gr1m0h-s-layout_source/keymap.c
+++ moonlander_gr1m0h-s-layout_mzdoX/moonlander_gr1m0h-s-layout_source/keymap.c
@@ -55,8 +55,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_6,                                           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       KC_EQUAL,       KC_BSPACE,      
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_BRIGHTNESS_UP,                                KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_LBRACKET,    KC_RBRACKET,    
     KC_CAPSLOCK,    KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_BRIGHTNESS_DOWN,                                                                KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,       KC_ENTER,       
-    KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_UP,          KC_SLASH,       
-    KC_LCTRL,       KC_LALT,        KC_LGUI,        KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,LGUI(LSFT(KC_4)),                                                                                                LCTL(LGUI(KC_SPACE)),KC_TILD,        KC_BSLASH,      KC_LEFT,        KC_DOWN,        KC_RIGHT,       
+    KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_UP,          KC_BSLASH,      
+    KC_LCTRL,       KC_LALT,        KC_LGUI,        KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,LGUI(LSFT(KC_4)),                                                                                                LCTL(LGUI(KC_SPACE)),KC_GRAVE,       KC_SLASH,       KC_LEFT,        KC_DOWN,        KC_RIGHT,       
     KC_SPACE,       KC_LANG2,       LCTL(LGUI(KC_Q)),                TG(1),          KC_LANG1,       KC_SPACE
   ),
   [1] = LAYOUT_moonlander(
